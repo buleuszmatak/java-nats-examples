167a49463a00759715b7661c559b85d01b91838e
Buleusz Matak <215200949+buleuszmatak@users.noreply.github.com>
2025-12-01 13:35:33 +0100

    Add auth-callout-bcfips

---
+++ auth-callout-bcfips/README.md
@@ -0,0 +1,7 @@
+![NATS](../images/large-logo.png)
+
+# Auth Callout with BCFIPS
+
+This example is an extension of the [Auth Callout](../auth-callout/README.md) example,
+that swaps out the default BouncyCastle security provider with the BCFIPS provider.
+This allows the example to run in FIPS mode.
---
+++ auth-callout-bcfips/src/main/java/io/nats/authcalloutbcfips/AuthCalloutServiceBcFipsExample.java
@@ -0,0 +1,44 @@
+package io.nats.authcalloutbcfips;
+
+import io.nats.authcallout.AuthCalloutServiceExample;
+import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
+
+import java.security.Provider;
+import java.security.Security;
+
+import static org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider.PROVIDER_NAME;
+
+public class AuthCalloutServiceBcFipsExample {
+    public static void main(String[] args) throws Exception {
+        setupBcFips();
+
+        // Normally set via JVM argument:
+        // -Dio.nats.client.security.provider=BCFIPS
+        System.setProperty("io.nats.client.security.provider", PROVIDER_NAME);
+        // or
+        // -Dio.nats.client.security.provider
+        // System.setProperty("io.nats.client.security.provider", "");
+        // if BCFIPS is configured as the default provider
+
+        AuthCalloutServiceExample.main(args);
+    }
+
+    private static void setupBcFips() {
+        // For test purposes. This is not a BCFIPS guide.
+        if (Security.getProvider(PROVIDER_NAME) != null) {
+            return;
+        }
+        Security.setProperty("ssl.KeyManagerFactory.algorithm", "PKIX");
+        Security.setProperty("ssl.TrustManagerFactory.algorithm", "PKIX");
+        Security.setProperty("org.bouncycastle.fips.approved_only", "true");
+
+        for (Provider otherProvider : Security.getProviders()) {
+            String providerName = otherProvider.getName();
+            if (!"SUN".equals(providerName)) {
+                Security.removeProvider(providerName);
+            }
+        }
+
+        Security.insertProviderAt(new BouncyCastleFipsProvider("C:DEFRND[SHA256];ENABLE{ALL};"), 1);
+    }
+}
